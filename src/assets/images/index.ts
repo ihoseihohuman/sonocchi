3b795cb7829a53b7c6b533c48b2bcf65d8849987
Sonoda-respecter <game_yo_s50@yahoo.co.jp>
2025-03-30 05:08:12 +0900

    画像のインポートを更新し、広告セクションの画像ソースを新しい画像に変更

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,14 +1,14 @@
 // src/assets/images/index.ts
-import image1 from './image1.jpg';
-import image2 from './image2.jpg';
-import image3 from './image3.jpg';
-import image4 from './image4.gif';
+import irisMysteria from './iris_mysteria.jpg';
+import misttrainGirlsX from './misttrain_girls_X.jpg';
+import pandaNatto from './panda_natto.jpg';
+import syokubutuNoSodatekata from './syokubutu_no_sodatekata.gif';
 
 const images: Record<string, string> = {
-    image1: image1,
-    image2: image2,
-    image3: image3,
-    image4: image4,
+    irisMysteria: irisMysteria,
+    misttrainGirlsX: misttrainGirlsX,
+    pandaNatto: pandaNatto,
+    syokubutuNoSodatekata: syokubutuNoSodatekata,
 };
 
 export default images;
