4552771b610d7235ed13816c511dc0dbf246163a
sonodaLover <takumi.kondo0108@gmail.com>
2026-02-23 07:16:52 +0900

    オーディオプレーヤーコンポーネントを追加し、Sonocchiページに統合

---
+++ src/components/molecules/audio_player/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const AudioPlayer: React.FC = () => {
+    return (
+        <div>
+            <audio controls>
+                <source src="/src/assets/musics/Perfect_Game_Professor.mp3" type="audio/mpeg" />
+                お使いのブラウザはオーディオ要素をサポートしていません。
+            </audio>
+        </div>
+    );
+};
+
+export default AudioPlayer;
---
+++ src/pages/sonocchi/index.tsx
@@ -0,0 +1,12 @@
+import AudioPlayer from '../../components/molecules/audio_player';
+
+const SonocchiPage: React.FC = () => {
+    return (
+        <div>
+            {/* ...existing content... */}
+            <AudioPlayer />
+        </div>
+    );
+};
+
+export default SonocchiPage;
