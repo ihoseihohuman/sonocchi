cc172bd662741ef33dbd4845bdd14df8cc83a0f2
sonodaLover <takumi.kondo0108@gmail.com>
2026-02-23 07:18:43 +0900

    SonocchiページにBGMコンポーネントを追加

--- src/components/pages/sonocchi/index.tsx
+++ src/components/pages/sonocchi/index.tsx
@@ -2,6 +2,7 @@ import Content from '@/components/organisms/sonocchi/content';
 import Popup from '@/components/organisms/sonocchi/popup';
 import Sidebar from '@/components/organisms/sonocchi/side_bar';
 import MainTemplate from '@/components/templates/sonocchi';
+import SonocchiPageBgm from '@/pages/sonocchi';
 import { Box } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 
@@ -10,6 +11,7 @@ export default function HomePage() {
 
     return (
         <MainTemplate title={t('sonocchi.header.title')}>
+            <SonocchiPageBgm />
             <Content />
             <Sidebar />
         </MainTemplate>
