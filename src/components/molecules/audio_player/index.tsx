d5a73d0bd1e0cd73277385bfa6e72ee8814f6a75
sonodaLover <takumi.kondo0108@gmail.com>
2026-02-23 07:16:57 +0900

    オーディオプレーヤーコンポーネントのコードを整形し、Sonocchiページに統合

--- src/components/molecules/audio_player/index.tsx
+++ src/components/molecules/audio_player/index.tsx
@@ -4,7 +4,10 @@ const AudioPlayer: React.FC = () => {
     return (
         <div>
             <audio controls>
-                <source src="/src/assets/musics/Perfect_Game_Professor.mp3" type="audio/mpeg" />
+                <source
+                    src="/src/assets/musics/Perfect_Game_Professor.mp3"
+                    type="audio/mpeg"
+                />
                 お使いのブラウザはオーディオ要素をサポートしていません。
             </audio>
         </div>
